c51616203764d13481c86d2203548170decaa141
Dave Huseby <dwh@linuxprogrammer.org>
2023-11-29 19:48:56 -0700

    update rust-multicodec dependency
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- src/serde/mod.rs
+++ src/serde/mod.rs
@@ -29,14 +29,14 @@ mod tests {
             &mk.compact(),
             &[
                 Token::Tuple { len: 5 },
-                Token::Bytes(&[0x3a]),       // Multikey sigil as varuint
-                Token::Bytes(&[0xed, 0x01]), // Ed25519Pub codec as varuint
-                Token::Bytes(&[0x00]),       // encrypted flag as varuint
-                Token::Seq { len: Some(0) }, // attributes array of varuints
+                Token::BorrowedBytes(&[0x3a]), // Multikey sigil as varuint
+                Token::BorrowedBytes(&[0xed, 0x01]), // Ed25519Pub codec as varuint
+                Token::BorrowedBytes(&[0x00]), // encrypted flag as varuint
+                Token::Seq { len: Some(0) },   // attributes array of varuints
                 Token::SeqEnd,
                 Token::Seq { len: Some(2) }, // data array of DataUnits
-                Token::Bytes(&[8, 116, 101, 115, 116, 32, 107, 101, 121]), // comment data unit
-                Token::Bytes(&[
+                Token::BorrowedBytes(&[8, 116, 101, 115, 116, 32, 107, 101, 121]), // comment data unit
+                Token::BorrowedBytes(&[
                     32, 62, 102, 130, 63, 172, 211, 84, 197, 123, 199, 162, 193, 229, 147, 156,
                     143, 179, 98, 26, 216, 202, 232, 167, 194, 234, 228, 168, 22, 121, 106, 131,
                     58,
@@ -81,20 +81,22 @@ mod tests {
             &mk.readable(),
             &[
                 Token::Struct {
-                    name: "Multikey",
+                    name: "multikey",
                     len: 4,
                 },
-                Token::Str("codec"),
+                Token::BorrowedStr("codec"),
                 Token::U64(0xed_u64),
-                Token::Str("encrypted"),
+                Token::BorrowedStr("encrypted"),
                 Token::Bool(false),
-                Token::Str("attributes"),
+                Token::BorrowedStr("attributes"),
                 Token::Seq { len: Some(0) },
                 Token::SeqEnd,
-                Token::Str("data"),
+                Token::BorrowedStr("data"),
                 Token::Seq { len: Some(2) },
-                Token::Str("f0874657374206b6579"),
-                Token::Str("f203e66823facd354c57bc7a2c1e5939c8fb3621ad8cae8a7c2eae4a816796a833a"),
+                Token::BorrowedStr("f0874657374206b6579"),
+                Token::BorrowedStr(
+                    "f203e66823facd354c57bc7a2c1e5939c8fb3621ad8cae8a7c2eae4a816796a833a",
+                ),
                 Token::SeqEnd,
                 Token::StructEnd,
             ],
@@ -139,25 +141,25 @@ mod tests {
             &mk2.compact(),
             &[
                 Token::Tuple { len: 5 },
-                Token::Bytes(&[0x3a]),       // Multikey sigil as varuint
-                Token::Bytes(&[0x80, 0x26]), // Ed25519Priv codec as varuint
-                Token::Bytes(&[0x01]),       // encrypted flag as varuint
-                Token::Seq { len: Some(6) }, // attributes array of varuints
-                Token::Bytes(&[141, 160, 3]),
-                Token::Bytes(&[10]),
-                Token::Bytes(&[1]),
-                Token::Bytes(&[165, 1]),
-                Token::Bytes(&[2]),
-                Token::Bytes(&[3]),
+                Token::BorrowedBytes(&[0x3a]), // Multikey sigil as varuint
+                Token::BorrowedBytes(&[0x80, 0x26]), // Ed25519Priv codec as varuint
+                Token::BorrowedBytes(&[0x01]), // encrypted flag as varuint
+                Token::Seq { len: Some(6) },   // attributes array of varuints
+                Token::BorrowedBytes(&[141, 160, 3]),
+                Token::BorrowedBytes(&[10]),
+                Token::BorrowedBytes(&[1]),
+                Token::BorrowedBytes(&[165, 1]),
+                Token::BorrowedBytes(&[2]),
+                Token::BorrowedBytes(&[3]),
                 Token::SeqEnd,
                 Token::Seq { len: Some(4) }, // data array of DataUnits
-                Token::Bytes(&[8, 116, 101, 115, 116, 32, 107, 101, 121]),
-                Token::Bytes(&[
+                Token::BorrowedBytes(&[8, 116, 101, 115, 116, 32, 107, 101, 121]),
+                Token::BorrowedBytes(&[
                     32, 98, 31, 32, 207, 218, 20, 11, 216, 191, 131, 168, 153, 22, 116, 40, 70, 41,
                     41, 164, 30, 155, 104, 168, 70, 123, 252, 36, 85, 233, 249, 132, 6,
                 ]),
-                Token::Bytes(&[8, 113, 78, 90, 191, 15, 123, 234, 232]),
-                Token::Bytes(&[
+                Token::BorrowedBytes(&[8, 113, 78, 90, 191, 15, 123, 234, 232]),
+                Token::BorrowedBytes(&[
                     64, 243, 97, 72, 175, 76, 149, 14, 238, 243, 148, 12, 215, 63, 136, 221, 255,
                     146, 10, 75, 114, 216, 245, 101, 27, 36, 109, 148, 134, 95, 103, 80, 122, 21,
                     141, 216, 46, 205, 183, 163, 76, 10, 125, 156, 69, 138, 131, 165, 216, 153,
@@ -207,28 +209,28 @@ mod tests {
             &mk2.readable(),
             &[
                 Token::Struct {
-                    name: "Multikey",
+                    name: "multikey",
                     len: 4,
                 },
-                Token::Str("codec"),
+                Token::BorrowedStr("codec"),
                 Token::U64(4864_u64),
-                Token::Str("encrypted"),
+                Token::BorrowedStr("encrypted"),
                 Token::Bool(true),
-                Token::Str("attributes"),
+                Token::BorrowedStr("attributes"),
                 Token::Seq { len: Some(6) },
-                Token::Str("f8da003"),
-                Token::Str("f0a"),
-                Token::Str("f01"),
-                Token::Str("fa501"),
-                Token::Str("f02"),
-                Token::Str("f03"),
+                Token::BorrowedStr("f8da003"),
+                Token::BorrowedStr("f0a"),
+                Token::BorrowedStr("f01"),
+                Token::BorrowedStr("fa501"),
+                Token::BorrowedStr("f02"),
+                Token::BorrowedStr("f03"),
                 Token::SeqEnd,
-                Token::Str("data"),
+                Token::BorrowedStr("data"),
                 Token::Seq { len: Some(4) },
-                Token::Str("f0874657374206b6579"),
-                Token::Str("f20621f20cfda140bd8bf83a899167428462929a41e9b68a8467bfc2455e9f98406"),
-                Token::Str("f08714e5abf0f7beae8"),
-                Token::Str("f40f36148af4c950eeef3940cd73f88ddff920a4b72d8f5651b246d94865f67507a158dd82ecdb7a34c0a7d9c458a83a5d899fe93fcf9ce37e337fab21dada48b89"),
+                Token::BorrowedStr("f0874657374206b6579"),
+                Token::BorrowedStr("f20621f20cfda140bd8bf83a899167428462929a41e9b68a8467bfc2455e9f98406"),
+                Token::BorrowedStr("f08714e5abf0f7beae8"),
+                Token::BorrowedStr("f40f36148af4c950eeef3940cd73f88ddff920a4b72d8f5651b246d94865f67507a158dd82ecdb7a34c0a7d9c458a83a5d899fe93fcf9ce37e337fab21dada48b89"),
                 Token::SeqEnd,
                 Token::StructEnd,
             ],
