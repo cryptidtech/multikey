0501e6c2b54d3742ece08bfc0faae8fa5600a57a
Dave Huseby <dwh@linuxprogrammer.org>
2023-12-05 16:44:11 -0700

    add Eq, Ord, PartialOrd to Nonce
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- src/nonce.rs
+++ src/nonce.rs
@@ -13,7 +13,7 @@ pub const SIGIL: Codec = Codec::Nonce;
 pub type EncodedNonce = BaseEncoded<Nonce>;
 
 /// a multicodec Nonce type
-#[derive(Clone, Default, PartialEq)]
+#[derive(Clone, Default, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Nonce {
     /// the random nonce bytes
     pub(crate) nonce: Vec<u8>,
